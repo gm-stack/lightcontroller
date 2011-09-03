fcdec04b5dddb8704e5f8cc55db5e768ae53589a
gm <gm@6219c1a7-eedc-4b83-93ca-c1744c1638e7>
2011-09-03 12:08:56 +0000

    update midilights.py to be a bit more flashy
    git-svn-id: http://svn.stackunderflow.com/svn/public/lightcontroller@252 6219c1a7-eedc-4b83-93ca-c1744c1638e7

--- misc/midilights.py
+++ misc/midilights.py
@@ -8,7 +8,13 @@ device = pypm.Input(1)
 
 timecount = 0
 
-colours = ["\x7F\x7F\x7F","\xFF\x00\x00","\x00\xFF\x00","\x00\x00\xFF"]
+def setLight(r,g,b):
+	r_c = chr(int(r))
+	g_c = chr(int(g))
+	b_c = chr(int(b))
+	return(r_c+g_c+b_c)
+
+colours = [(127,127,127),(255,0,0),(0,255,0),(0,0,255)]
 
 sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
 
@@ -19,10 +25,15 @@ while 1:
         midicode =  msg[0][0][0]
         if midicode == 248:
             timecount += 1
+            fracbeat = timecount % 24
             beat = int(timecount/24)
             beatinbar = int(timecount/24)%4
-            print "%i:%i" % (beat,beatinbar)
-            sock.sendto(colours[beatinbar],("172.22.0.55",54127))
+            #print "%i:%i:%i" % (beat,beatinbar,fracbeat)
+            colourmult = (23.0-fracbeat)/23.0
+            r = colours[beatinbar][0]*colourmult
+            g = colours[beatinbar][1]*colourmult
+            b = colours[beatinbar][2]*colourmult
+            sock.sendto(setLight(r,g,b),("172.22.0.55",54127))
             
 
             
